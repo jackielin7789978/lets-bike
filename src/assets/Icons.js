8fdc7e4953f4a752bcbec90b5a60176efb3c43c6
jackie <jackielin7789978@gmail.com>
2021-11-16 01:01:20 +0800

    add icons

---
+++ src/assets/Icons.js
@@ -0,0 +1,47 @@
+import { ReactComponent as Address } from './svgs/address.svg'
+import { ReactComponent as Attraction } from './svgs/attraction.svg'
+import { ReactComponent as Bike } from './svgs/bike.svg'
+import { ReactComponent as Down } from './svgs/chevronDown.svg'
+import { ReactComponent as Up } from './svgs/chevronUp.svg'
+import { ReactComponent as Closed } from './svgs/closed.svg'
+import { ReactComponent as GoBack } from './svgs/goBack.svg'
+import { ReactComponent as Loading } from './svgs/loading.svg'
+import { ReactComponent as Map } from './svgs/map.svg'
+import { ReactComponent as Mark } from './svgs/mark.svg'
+import { ReactComponent as Phone } from './svgs/phone.svg'
+import { ReactComponent as Position } from './svgs/position.svg'
+import { ReactComponent as Remove } from './svgs/remove.svg'
+import { ReactComponent as Route } from './svgs/route.svg'
+import { ReactComponent as Search } from './svgs/search.svg'
+import { ReactComponent as Setting } from './svgs/setting.svg'
+import { ReactComponent as Station } from './svgs/station.svg'
+import { ReactComponent as Time } from './svgs/time.svg'
+import { ReactComponent as Update } from './svgs/update.svg'
+import { ReactComponent as Web } from './svgs/web.svg'
+import { ReactComponent as ZoomIn } from './svgs/zoomIn.svg'
+import { ReactComponent as ZoomOut } from './svgs/zoomOut.svg'
+
+export const ICONS = {
+  Address,
+  Attraction,
+  Bike,
+  Down,
+  Up,
+  Closed,
+  GoBack,
+  Loading,
+  Map,
+  Mark,
+  Phone,
+  Position,
+  Remove,
+  Route,
+  Search,
+  Setting,
+  Station,
+  Time,
+  Update,
+  Web,
+  ZoomIn,
+  ZoomOut
+}
