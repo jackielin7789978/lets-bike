ec4ce0bdd2c8801c649187d8e1414eb4433866ba
jackie <jackielin7789978@gmail.com>
2021-11-21 00:59:51 +0800

    add ApiContext + lift states up to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import GlobalStyle from './constants/globalStyle'
 import { THEME } from './constants/styles'
 import { ThemeProvider } from 'styled-components'
@@ -9,28 +9,99 @@ import {
   Stations,
   NotFound
 } from './components/pages'
-import { NavContext } from './contexts'
+import { NavContext, ApiContext } from './contexts'
+import BikeApi from './webAPIs'
 
 export default function App() {
   const [navMenu, setNavMenu] = useState('map')
   const [isCardOpen, setIsCardOpen] = useState(false)
 
+  const [myPosition, setMyPosition] = useState(null)
+  const [mapInstance, setMapInstance] = useState(null)
+  const [mapApi, setMapApi] = useState(null)
+
+  const [stations, setStations] = useState([])
+
+  const stationWithStatus = (stationArr, statusArr) => {
+    return stationArr.map((station) => {
+      const status = statusArr.filter(
+        (status) => status.StationUID === station.StationUID
+      )
+      return {
+        ...station,
+        status: status[0]
+      }
+    })
+  }
+
+  const getCenterCoords = useCallback(() => {
+    if (!mapInstance) return
+    const lat = mapInstance.center.lat()
+    const lng = mapInstance.center.lng()
+    console.log(lat, lng)
+    return [lat, lng]
+  }, [mapInstance])
+
+  const findNearbyStations = useCallback(() => {
+    if (!mapInstance) return
+    const [lat, lng] = getCenterCoords()
+    const nearby = `nearby(${lat}, ${lng}, 700)`
+
+    const axiosOptions = {
+      params: {
+        $top: 10,
+        $spatialFilter: nearby
+      }
+    }
+
+    ;(async () => {
+      let resStation
+      let resStatus
+      try {
+        resStation = await BikeApi.get('/Station/NearBy', axiosOptions)
+        resStatus = await BikeApi.get('/Availability/NearBy', axiosOptions)
+        console.log(stationWithStatus(resStation.data, resStatus.data))
+        setStations(() => stationWithStatus(resStation.data, resStatus.data))
+      } catch (err) {
+        console.log(err)
+      }
+    })()
+  }, [getCenterCoords, mapInstance])
+
+  useEffect(() => {
+    if (!myPosition) return
+    findNearbyStations()
+  }, [findNearbyStations, myPosition])
+
   return (
     <Router>
-      <NavContext.Provider
-        value={{ navMenu, setNavMenu, isCardOpen, setIsCardOpen }}
+      <ApiContext.Provider
+        value={{
+          mapInstance,
+          setMapInstance,
+          mapApi,
+          setMapApi,
+          myPosition,
+          setMyPosition,
+          stations,
+          findNearbyStations
+        }}
       >
-        <ThemeProvider theme={THEME}>
-          <GlobalStyle />
-          <Routes>
-            <Route path='/' element={<Map />} />
-            <Route path='/map' element={<Map />} />
-            <Route path='/stations' element={<Stations />} />
-            <Route path='/routes' element={<RoutesPage />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </ThemeProvider>
-      </NavContext.Provider>
+        <NavContext.Provider
+          value={{ navMenu, setNavMenu, isCardOpen, setIsCardOpen }}
+        >
+          <ThemeProvider theme={THEME}>
+            <GlobalStyle />
+            <Routes>
+              <Route path='/' element={<Map />} />
+              <Route path='/map' element={<Map />} />
+              <Route path='/stations' element={<Stations />} />
+              <Route path='/routes' element={<RoutesPage />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </ThemeProvider>
+        </NavContext.Provider>
+      </ApiContext.Provider>
     </Router>
   )
 }
--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -1,11 +1,11 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useContext, useEffect, useCallback } from 'react'
+import { ApiContext } from '../../contexts'
 import { useNavigate } from 'react-router'
 import styled from 'styled-components'
 import GoogleMapReact from 'google-map-react'
 import { GOOGLE_API_KEY } from '../../key'
 import mapStyles from '../../constants/mapStyles'
 import { RefreshBTN, SettingBTN, PositionBTN } from '../Buttons'
-import BikeApi from '../../webAPIs'
 import Mark from '../Mark'
 import Navbar from '../Navbar'
 
@@ -37,10 +37,16 @@ export default function Map() {
   const navigate = useNavigate()
   useEffect(() => navigate('/map'), [navigate])
 
-  const [myPosition, setMyPosition] = useState(null)
-  const [mapInstance, setMapInstance] = useState(null)
-  const [mapApi, setMapApi] = useState(null)
-  const [stations, setStations] = useState([])
+  const {
+    mapInstance,
+    setMapInstance,
+    mapApi,
+    setMapApi,
+    myPosition,
+    setMyPosition,
+    stations,
+    findNearbyStations
+  } = useContext(ApiContext)
 
   const apiHasLoaded = (map, maps) => {
     setMapInstance(map)
@@ -72,56 +78,7 @@ export default function Map() {
       },
       options
     )
-  }, [mapApi, mapInstance])
-
-  const getCenterCoords = useCallback(() => {
-    const lat = mapInstance.center.lat()
-    const lng = mapInstance.center.lng()
-    return [lat, lng]
-  }, [mapInstance?.center])
-
-  const stationWithStatus = (stationArr, statusArr) => {
-    return stationArr.map((station) => {
-      const status = statusArr.filter(
-        (status) => status.StationUID === station.StationUID
-      )
-      return {
-        ...station,
-        status: status[0]
-      }
-    })
-  }
-
-  const findNearbyStations = useCallback(() => {
-    if (!mapApi && !myPosition) return
-    const [lat, lng] = getCenterCoords()
-    const nearby = `nearby(${lat}, ${lng}, 700)`
-
-    const axiosOptions = {
-      params: {
-        $top: 10,
-        $spatialFilter: nearby
-      }
-    }
-
-    ;(async () => {
-      let resStation
-      let resStatus
-      try {
-        resStation = await BikeApi.get('/Station/NearBy', axiosOptions)
-        resStatus = await BikeApi.get('/Availability/NearBy', axiosOptions)
-        console.log(stationWithStatus(resStation.data, resStatus.data))
-        setStations(() => stationWithStatus(resStation.data, resStatus.data))
-      } catch (err) {
-        console.log(err)
-      }
-    })()
-  }, [getCenterCoords, mapApi, myPosition])
-
-  useEffect(() => {
-    if (!myPosition) return
-    findNearbyStations()
-  }, [findNearbyStations, myPosition])
+  }, [mapApi, mapInstance, setMyPosition])
 
   useEffect(() => {
     getUserLocation()
@@ -134,7 +91,6 @@ export default function Map() {
   const handleCenterChange = () => {
     findNearbyStations()
   }
-
   const handleReposition = () => {
     mapInstance.setCenter(myPosition)
   }
